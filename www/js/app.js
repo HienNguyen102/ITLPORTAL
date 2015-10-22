ca67ce2b04f87b0cabb2f76379b426d45f186fb9
HienNguyen102 <nguyenconghien0102@gmail.com>
2015-10-22 08:50:15 -0700

    Thêm chức năng complaint

--- www/js/app.js
+++ www/js/app.js
@@ -75,10 +75,161 @@ app.config(function ($stateProvider, $urlRouterProvider) {
                 controller: 'ViewMeetingCtrl',
             }
         }
+    })
+  //Cau hinh route cho module complaint
+  // Route cua complaint list
+    .state('menu.tabs.complaints', {
+        url: "/complaints",
+        views: {
+            'view-tab': {
+                templateUrl: "templates/complaint/listview.html",
+                controller: 'ListComplaintCtrl',
+            }
+        }
     });
     
   $urlRouterProvider.otherwise("menu/tab/meetings");
 
 })
+// Cac ham xu ly trong controller
+.controller('ListCtrl', function ($scope) {
+
+  $scope.data = {
+    showDelete: false
+  };
+
+  $scope.itemButtons = [
+    {
+      text: 'Delete',
+      type: 'button-assertive',
+      onTap: function (item) {
+        alert('Delete Item: ' + item.id + ' ?');
+      }
+    }
+  ];
+
+  $scope.onItemDelete = function (item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+
+  $scope.items = [
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+    {
+      id: 4
+    },
+    {
+      id: 5
+    },
+    {
+      id: 6
+    },
+    {
+      id: 7
+    },
+    {
+      id: 8
+    },
+    {
+      id: 9
+    },
+    {
+      id: 10
+    }
+  ];
+
+})
+
+.controller('ButtonsTabCtrl', function ($scope, $ionicPopup, $ionicActionSheet, $ionicModal) {
+    $scope.showPopup = function () {
+     $ionicPopup.alert({
+       title: 'Popup',
+       content: 'This is ionic popup alert!'
+     });
+    };
+    $scope.showActionsheet = function () {
+        $ionicActionSheet.show({
+          titleText: 'Ionic ActionSheet',
+          buttons: [
+            {
+              text: 'Facebook'
+            },
+            {
+              text: 'Twitter'
+            },
+          ],
+          destructiveText: 'Delete',
+          cancelText: 'Cancel',
+          cancel: function () {
+            console.log('CANCELLED');
+          },
+          buttonClicked: function (index) {
+            console.log('BUTTON CLICKED', index);
+            return true;
+          },
+          destructiveButtonClicked: function () {
+            console.log('DESTRUCT');
+            return true;
+          }
+        });
+    };
+})
+
+.controller('SlideboxCtrl', function($scope, $ionicSlideBoxDelegate) {
+  $scope.nextSlide = function() {
+    $ionicSlideBoxDelegate.next();
+  }             
+})              
+
+.controller('MenuCtrl', function($scope, $ionicSideMenuDelegate, $ionicModal) {              
+  $ionicModal.fromTemplateUrl('templates/modal.html', function (modal) {
+    $scope.modal = modal;
+  }, {
+    animation: 'slide-in-up'
+  });
+    $ionicModal.fromTemplateUrl('templates/meeting/editview.html', function (meetingModal) {
+    $scope.meetingModal = meetingModal;
+  }, {
+    animation: 'slide-in-up'
+  });
+ })
+  
+ .controller('AppCtrl', function() {
+
+  ionic.Platform.ready(function() {
+
+  });
+
+ })
+ .controller('CreateMeetingCtrl', function($scope) {
+    $scope.send = function(data){
+        alert(data.title + data.location);
+    }
+})
+// Xu ly cua controller ListMeetingCrtl
+ .controller('ListMeetingCtrl', function($scope, $ionicSideMenuDelegate, $ionicModal) {
+    // Khoi tao form nhap lieu khi nguoi dung den trang list view cua meeting, khi nguoi dung
+    // nhan nut tao o phia tren goc phai thi hien thi form len cho nhap lieu
+    $ionicModal.fromTemplateUrl('templates/meeting/editview.html', function (meetingModal) {
+    $scope.meetingModal = meetingModal;
+  }, {
+    animation: 'slide-in-up'
+  });
+ })
+// Xu ly cua controller ListComplaintCtrl
+ .controller('ListComplaintCtrl', function($scope, $ionicSideMenuDelegate, $ionicModal) {
+    $ionicModal.fromTemplateUrl('templates/meeting/editview.html', function (meetingModal) {
+    $scope.meetingModal = meetingModal;
+  }, {
+    animation: 'slide-in-up'
+  });
+ });
               
               
