4e320281f4163a30830868ee3b7fa7032a627e9c
HienNguyen <nguyenconghien0102@gmail.com>
2015-12-12 21:11:36 -0800

    Add MapMenuCtrl

--- www/js/map_controller.js
+++ www/js/map_controller.js
@@ -16,7 +16,7 @@ app.controller('MapCtrl', function ($scope, $ionicLoading, $stateParams, $cordov
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    debugger;
+    //debugger;
     var map = new google.maps.Map(document.getElementById("map"),
         mapOptions);
     var geocoder = new google.maps.Geocoder();
@@ -41,13 +41,64 @@ app.controller('MapCtrl', function ($scope, $ionicLoading, $stateParams, $cordov
                         type: 'button-positive',
                         onTap: function (e) {
                             $ionicHistory.goBack();
-                        }
+        }
       },
                     {
                         text: 'Hủy'
                     }
     ]
-            });
+    });
         }
     });
+});
+app.controller('MapMenuCtrl', function ($scope, $ionicLoading, $stateParams, $cordovaGeolocation) {
+    /*if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+
+    function showPosition(position) {
+        alert("Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude)
+    }*/
+    $scope.getLocation = function () {
+        console.log("getLoation()");
+        /*var posOptions = {
+            enableHighAccuracy: false
+        };
+        $cordovaGeolocation
+            .getCurrentPosition(posOptions)
+            .then(function (position) {
+                var lat = position.coords.latitude
+                var long = position.coords.longitude
+                alert("lat: " + lat + " long: " + long);
+            }, function (err) {
+                alert("loi");
+            });*/
+        /*plugin.google.maps.external.launchNavigation({
+            "from": "Tokyo, Japan",
+            "to": "Kyoto, Japan"
+        });*/
+        var map;
+        var div = document.getElementById("map_canvas");
+
+        // Initialize the map view
+        map = plugin.google.maps.Map.getMap(div);
+
+        // Wait until the map is ready status.
+        map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+    }
+
+    function onMapReady() {
+        /*var button = document.getElementById("button");
+        button.addEventListener("click", onBtnClicked, false);*/
+        alert("onMapReady");
+    }
+
+    /*function onBtnClicked() {
+        map.showDialog();
+    }*/
+
+
 });
